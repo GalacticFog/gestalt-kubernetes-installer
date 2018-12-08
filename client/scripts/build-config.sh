35d2152d17ef3353ad4d2e7850ffa231aba8ac36
Rasma Abolina <rasma@galacticfog.com>
2018-12-08 12:10:08 -0500

    add kong version customization

--- client/scripts/build-config.sh
+++ client/scripts/build-config.sh
@@ -57,7 +57,7 @@ cat >> ${GENERATED_CONF_FILE} << EOF
     "JS_EXECUTOR_IMAGE": "${docker_registry}/gestalt-laser-executor-js:${gestalt_docker_release_tag}",
     "JS_EXECUTOR_MIN_COOL": "${gestalt_laser_executor_js_mincool:-0}",
     "JVM_EXECUTOR_IMAGE": "${docker_registry}/gestalt-laser-executor-jvm:${gestalt_docker_release_tag}",
-    "KONG_IMAGE": "${docker_registry}/kong:${gestalt_docker_release_tag}",
+    "KONG_IMAGE": "${docker_registry}/kong:${gestalt_kong_release_tag:-gestalt_docker_release_tag}",
     "KONG_INGRESS_SERVICE_NAME": "kng-ext",
     "KONG_NODEPORT": "${gestalt_kong_service_nodeport}",
     "KONG_MANAGEMENT_NODEPORT": "${gestalt_kong_management_nodeport}",
