0489f977098ec29f5ac0af2bab95630238da1b77
Eric Sonchaiwanich <eric@galacticfog.com>
2018-12-10 18:06:56 -0500

    Rearrange folders

--- client/scripts/utilities/utility-gestalt-installer-run.sh
+++ client/scripts/utilities/utility-gestalt-installer-run.sh
@@ -49,27 +49,28 @@ gestalt_install_validate_preconditions() {
 gestalt_install_create_configmaps() {
 
   # Create configmap for install data
-  tar cfzv ./configmaps/install-data.tar.gz -C .. \
+  mkdir tmp && \
+  tar cfzv ./tmp/install-data.tar.gz -C .. \
     gestalt \
     resource_templates \
     scripts \
     config \
-    && cat ./configmaps/install-data.tar.gz | base64 > ./configmaps/install-data.tar.gz.b64
-  cmd="kubectl create configmap -n ${kube_namespace} install-data --from-file ./configmaps/install-data.tar.gz.b64"
+    && cat ./tmp/install-data.tar.gz | base64 > ./tmp/install-data.tar.gz.b64
+  cmd="kubectl create configmap -n ${kube_namespace} install-data --from-file ./tmp/install-data.tar.gz.b64"
   echo $cmd
   $cmd
   exit_on_error "Failed create configmap from resource_templates directory, aborting."
 
   # for CACERTS file
-  if [ -f configmaps/cacerts ]; then
+  echo "TODO: Ensure cacerts is handled properly"
+  if [ -f tmp/cacerts ]; then
     echo "Creating 'gestalt-security-cacerts' configmap from $gestalt_security_cacerts_file..."
     kubectl create configmap -n ${kube_namespace} gestalt-security-cacerts --from-file=configmaps/cacerts
     exit_on_error "Failed to build gestalt configmap data"
   fi
 
   # Cleanup
-  rm configmaps/*.tar.gz
-  rm configmaps/*.tar.gz.b64
+  rm -r ./tmp
 }
 
 # Run the install container with ConfigMaps
