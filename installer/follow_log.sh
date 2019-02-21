7a49769032636f17df4be15201edb3a3718acc58
Sebastian Bernheim <sebastian@galacticfog.com>
2019-02-21 16:27:35 -0500

    Fixing follow_log.sh tool

--- installer/follow_log.sh
+++ installer/follow_log.sh
@@ -1,2 +1,7 @@
 . gestalt.conf
+. helpers/tool-functions.sh
+
+RELEASE_NAMESPACE=$( find_namespace ${RELEASE_NAMESPACE} )
+RELEASE_NAME=$( find_release ${RELEASE_NAME} )
+
 kubectl logs -n ${RELEASE_NAMESPACE} ${RELEASE_NAME}-installer --follow
