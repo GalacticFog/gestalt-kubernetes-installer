c975e6d1398190256317bbd82a21a26f4035cabf
Sebastian Bernheim <sebastian@galacticfog.com>
2019-02-28 09:29:48 -0500

    Don't check for removed (unused) installer.yaml file.

--- installer/scripts/utilities/utility-gestalt-installer-run.sh
+++ installer/scripts/utilities/utility-gestalt-installer-run.sh
@@ -4,10 +4,6 @@ gestalt_install_validate_preconditions() {
 
     # TODO: Make debug echo "Expectation, that user already successfully ran ./configure.sh"
 
-    file_array="${INSTALLER_CONFIG_FILE}"
-    log_debug "[${FUNCNAME[0]}] Check for required files: '${file_array[@]}'"
-    check_for_required_files "${file_array[@]}"
-
     log_debug "[${FUNCNAME[0]}] Validate that expected environment variables are set ..."
 
     check_for_required_variables \
